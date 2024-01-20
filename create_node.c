132dc06aa6fd6f2f0a27bce2713fbd75dfa8a5db
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-01-20 03:37:45 -0300

    feat(linked-lsit): add function to create node

---
+++ create_node.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_node.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 01:09:09 by guribeir          #+#    #+#             */
+/*   Updated: 2024/01/20 01:22:11 by guribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "hello.h"
+
+t_node	*create_node(char c)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->letter = c;
+	node->next = NULL;
+	return (node);
+}
