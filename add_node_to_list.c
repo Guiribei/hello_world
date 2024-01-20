93d346e137d019c65d2c30b88b714524631db9a8
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-01-20 03:37:00 -0300

    feat(linked-list): add function to put a node at the end of the list

---
+++ add_node_to_list.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_node_to_list.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 01:15:33 by guribeir          #+#    #+#             */
+/*   Updated: 2024/01/20 02:50:02 by guribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "hello.h"
+
+void	add_node_to_list(t_node **head, t_node *new_node)
+{
+	t_node	*tmp;
+
+	tmp = *head;
+	if (!new_node)
+		return ;
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_node;
+	}
+	return ;
+}
